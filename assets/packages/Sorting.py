952836fd2cca1361c769637b2f10af186f41562c
DevPyCyber <c.mazet007@gmail.com>
2020-10-31 19:15:55 +0100

    update

--- assets/packages/Sorting.py
+++ assets/packages/Sorting.py
@@ -16,28 +16,35 @@ class sorting:
         self.file_path = ''
         self.path_config = 'C:/Users/cmaze/OneDrive/developpement/python/projets systeme/File-sorter-and-security/configuration'
         self.parser = ConfigParser()
-        self.content = None
-        self.method_ext = None
-        self.method_lett = None
-        self.path_letters = None
-        self.paths_ext = None
-        self.pl = None
+        self.method_ext = ''
+        self.method_lett = ''
+        self.list_path_lett = {}
+        self.list_path_ext = {}
+
+        self.list_file = []
+
 
     def get_info_config_ini(self):
         os.chdir(self.path_config)
         self.parser.read('config.ini')
         self.method_ext = self.parser.get("method", "extension")
         self.method_lett = self.parser.get("method", "letters")
-        
-                
+        if self.method_lett == 'True':
+            for keys, values in self.parser.items("paths letters"):
+                self.list_path_lett.update({str(keys):str(values)})
 
-sort = sorting()
-sort.get_info_config_ini()
+        elif self.method_ext == 'True':
+            for k, v in self.parser.items("paths extension"):
+                self.list_path_ext.update({str(k): str(v)})
+        print(self.list_path_lett)
 
-        
+    def browse_folder(self):
+        for keys in self.list_path_ext.items():
+            self.list_file.update()
             
 
-
+            
+                
 sorter = sorting()
 sorter.get_info_config_ini()
 
