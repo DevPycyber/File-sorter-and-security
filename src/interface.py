686beb46608ed0849c86438a3a2326e602607fc4
DevPyCyber <c.mazet007@gmail.com>
2020-11-28 18:15:18 +0100

    "modif"

--- src/interface.py
+++ src/interface.py
@@ -1,11 +1,8 @@
-  
 import tkinter
 from tkinter import filedialog
 import json
-from assets.packages.Sorting import sorting
+
 from tkinter import BooleanVar
-
-
 
 
 class Interface:
@@ -25,9 +22,10 @@ class Interface:
 
         self.button_check_letters = tkinter.Checkbutton(self.main_windows, text="trier grâce aux lettres composant le nom du fichier",
         font=("Arial", 20), variable=self.check_letters)
-        self.paths_extensions = tkinter.Text(self.main_windows, height=5, state="disabled")
-        self.paths_letters = tkinter.Text(self.main_windows, height=5, state="disabled") 
-        self.browse_button = tkinter.Button(self.main_windows, text="browse", font=("Arial", 15), state="disabled")
+        self.paths_extensions = tkinter.Text(self.main_windows,height=5, state="normal")
+        self.paths_letters = tkinter.Text(self.main_windows,height=5, state="normal") 
+        self.browse_button = tkinter.Button(self.main_windows, text="browse", font=("Arial", 15), state="normal")
+        self.label_dir = tkinter.Label(self.main_windows)
 
     def place_widgets(self):
         self.browse_button.place(x=700, y=150)
@@ -36,23 +34,29 @@ class Interface:
         self.button_check_letters.place(x=200, y=250)
         self.presentation.pack(anchor='n')
         self.browse_button.place()
+        
     
     #fonctions check buttons
 
-    def change_app_ext(self):
-        if self.paths_extensions.cget("state") == 'normal':
-            self.paths_extensions.config(state='disabled')
-        else:
-            self.paths_extensions.config(state='normal')
-
-    def change_app_lett(self):
-        if self.paths_letters.cget("state") == 'normal':
-            self.paths_letters.config(state='disabled')
-        else:
-            self.paths_letters.config(state='normal')
-
     def browse_directory(self):
-        self.directory_name = filedialog.askdirectory(initialdir = "/",
-                                            title="Select a folder")
-        return self.directory_name
-            
+        self.directory_name = filedialog.askdirectory(title="select a folder")
+        self.label_dir.config(text=self.directory_name, font=("Arial", 10))
+        self.label_dir.pack(side="bottom")
+        self.paths_extensions.insert(0, self.directory_name)
+
+    def config_buttons(self):
+        self.browse_button.config(command= lambda: Interface.browse_directory(self))
+
+a = Interface(700, 1000, "first")
+a.place_widgets()
+a.config_buttons()
+a.main_windows.mainloop()
+
+
+
+
+
+
+
+
+        
--- test.py
+++ test.py
@@ -1,12 +1,7 @@
+#Importation des modules
+
+from tkinter.filedialog import askdirectory
 import tkinter as tk
-from tkinter import IntVar, StringVar, BooleanVar
 
-main = tk.Tk()
 
-def get_value():
-    print(tk1.get())
 
-tk1 = BooleanVar()
-B1 = tk.Checkbutton(main, text="active", variable=tk1, command=get_value)
-B1.pack()
-main.mainloop()
