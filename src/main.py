f037c60d0edc56e1a7d182acf745b18fa2478605
DevPyCyber <c.mazet007@gmail.com>
2020-12-05 18:05:37 +0100

    remise a niveau

---
+++ src/main.py
@@ -0,0 +1,21 @@
+#importation des différents modules
+
+
+
+#affichage de la fenêtre
+
+
+
+
+#envoie des informations vers config.ini
+
+
+
+
+#configuration des temps d'attente
+
+
+
+
+
+#tri automatique
--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 #Importation des modules
 
 from tkinter.filedialog import askdirectory
-import tkinter as tk
+from tkinter import*
+
+root = Tk()
+scrool = Scrollbar()
+scrool.pack(side=RIGHT, fill=Y)
+
+liste = Listbox(root, yscrollcommand = scrool.set)
+for a in range(1, 100):
+    liste.insert(END, "hello world" + str(a))
+liste.pack(side=LEFT, fill=BOTH)
+
+scrool.config(command=liste.yview)
+
+root.mainloop()
+
+
 
 
 
